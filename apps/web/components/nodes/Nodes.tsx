869d805be3ac771fd2c8a8aed315df8312aea373
vinit <vinitk81144@gmail.com>
2025-11-06 00:42:40 +0530

    Modified Gemini node

--- apps/web/components/nodes/Nodes.tsx
+++ apps/web/components/nodes/Nodes.tsx
@@ -1,8 +1,28 @@
 "use client";
 
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Handle, Position } from "reactflow";
 import "reactflow/dist/style.css";
+import { IconPointer } from "@tabler/icons-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 import { Button } from "../ui/button";
 
 // type NodeData = {
@@ -50,16 +70,20 @@ function TriggerManually({
 }: {
   id: string;
   data: {
-    onSend: (id: string, payload: { trigger: boolean }) => void;
+    onSend: (id: string, payload: { proceed: boolean }) => void;
   };
 }) {
   const handleClick = useCallback(() => {
-    data.onSend(id, { trigger: true });
+    data.onSend(id, { proceed: true });
   }, [data, id]);
 
   return (
-    <div>
-      <Button onClick={handleClick}>Trigger Workflow</Button>
+    <div
+      className="border p-4 bg-[#262626] rounded-2xl cursor-pointer"
+      onClick={handleClick}
+    >
+      <IconPointer size={35} />
+      <pre className="font-semibold">Click</pre>
       <Handle type="source" position={Position.Right} />
     </div>
   );
@@ -72,13 +96,18 @@ function GeminiNode({
   id: string;
   data: {
     onSend: (id: string, payload: string) => void;
+    received: {
+      proceed: boolean;
+    };
   };
 }) {
   const promptRef = useRef<HTMLInputElement>(null);
   const apiKeyRef = useRef<HTMLInputElement>(null);
-  const modelRef = useRef<HTMLInputElement>(null);
+  const [prompt, setPrompt] = useState(promptRef.current?.value);
+  const [apiKey, setApiKey] = useState(promptRef.current?.value);
+  const [model, setModel] = useState("gemini-2.5-flash");
 
-  const handleClick = useCallback(async () => {
+  const askGemini = useCallback(async () => {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -86,35 +115,98 @@ function GeminiNode({
       method: "POST",
       headers: myHeaders,
       body: JSON.stringify({
-        prompt: promptRef.current ? promptRef.current.value : "",
-        apiKey: apiKeyRef.current ? apiKeyRef.current.value : "",
-        model: modelRef.current ? modelRef.current.value : "",
+        prompt: prompt,
+        apiKey: apiKey,
+        model: model,
       }),
       redirect: "follow",
     });
     const result = (await response.json()).result;
-    console.log(result);
-    data.onSend(id, result);
-  }, [data, id]);
+    onSendRef.current?.(id, result);
+  }, [apiKey, prompt, model, id]);
+
+  const onSendRef = useRef(data.onSend);
+  useEffect(() => {
+    onSendRef.current = data.onSend;
+  }, [data.onSend]);
+
+  useEffect(() => {
+    if (data.received?.proceed === true) {
+      askGemini();
+      data.received.proceed = false;
+    }
+  }, [data.received, askGemini]);
 
   return (
-    <div className="flex flex-col justify-center items-center gap-4 bg-[#262626] p-4 rounded-2xl">
-      <input
-        ref={promptRef}
-        placeholder="Prompt"
-        className="border border-dashed border-white rounded-md p-2"
-      />
-      <input
-        ref={apiKeyRef}
-        placeholder="ApiKey"
-        className="border border-dashed border-white rounded-md p-2"
-      />
-      <input
-        ref={modelRef}
-        placeholder="Model"
-        className="border border-dashed border-white rounded-md p-2"
-      />
-      <Button onClick={handleClick}>Ask Gemini</Button>
+    <div className="bg-[#262626] p-4 rounded-2xl">
+      <Dialog>
+        <DialogTrigger>
+          <Image
+            src={"gemini-color.svg"}
+            alt="Gemini"
+            width={50}
+            height={50}
+            className="cursor-pointer"
+          />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Gemini Node options</DialogTitle>
+            <DialogTrigger className="flex justify-start">
+              <pre>To get Gemini api-key visit: </pre>
+              <Link
+                href={"https://aistudio.google.com/app/apikey"}
+                className="text-blue-500"
+              >
+                Google AI Studio
+              </Link>
+            </DialogTrigger>
+            <DialogDescription>
+              These options will be used when this node is trigerred
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col justify-center items-center gap-4 p-4 rounded-2xl">
+            <input
+              ref={promptRef}
+              placeholder="Prompt"
+              className="border border-dashed border-white rounded-md p-2"
+              defaultValue={prompt}
+            />
+            <input
+              ref={apiKeyRef}
+              placeholder="ApiKey"
+              className="border border-dashed border-white rounded-md p-2"
+              defaultValue={apiKey}
+            />
+            <Select
+              onValueChange={(value: string) => setModel(value)}
+              defaultValue="gemini-2.5-flash"
+            >
+              <SelectTrigger className="w-[270px]">
+                <SelectValue placeholder="Models" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Models</SelectLabel>
+                  <SelectItem value="gemini-2.5-flash">
+                    gemini-2.5-flash
+                  </SelectItem>
+                  <SelectItem value="gemini-2.5-pro">gemini-2.5-pro</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Button
+              onClick={() => {
+                setApiKey(apiKeyRef.current?.value as string);
+                setPrompt(promptRef.current?.value as string);
+              }}
+            >
+              Save values
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+      <Handle type="target" position={Position.Left} />
       <Handle type="source" position={Position.Right} />
     </div>
   );
@@ -122,7 +214,7 @@ function GeminiNode({
 
 function ShowOutput({ data }: { data: { received: JSON } }) {
   return (
-    <div className="p-2 border rounded bg-[#262626]">
+    <div className="p-2 border rounded-2xl bg-[#262626]">
       <div>Show Output</div>
       <pre className="text-xs p-1 rounded max-w-48 text-wrap break-words">
         {JSON.stringify(data.received, null, 2) || ""}
